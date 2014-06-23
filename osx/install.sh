0b3dbdd9e9b63358a5f5bcf9f8734df3435573e4
Eren Türkay <turkay.eren@gmail.com>
2014-06-23 15:05:09 +0300

    osx: install.sh: add information on the process

--- osx/install.sh
+++ osx/install.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+
+echo 'Copying Programmer Qwerty.bundle to ~/Library/Keyboard Layouts'
 cp -R ./Programmer\ Qwerty.bundle ~/Library/Keyboard\ Layouts/
+echo 'Install complete!\n'
+echo 'Please enable Programmer Qwerty in Keyboard Input Sources then
+logoff/login again. Enjoy!'
